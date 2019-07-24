4e57f78b985c811bc61e27f0ea4ac632dafc27cd
hwm09110 <771734186@qq.com>
2019-07-24 11:33:17 +0800

    update

--- src/utils/wxApiPromise.js
+++ src/utils/wxApiPromise.js
@@ -3,7 +3,13 @@ import toPromise from './toPromise'
 const toPromiseWx = toPromise(wx)
 
 const getImageInfo = toPromiseWx('getImageInfo')
+const getUserInfo = toPromiseWx('getUserInfo')
+const getSetting = toPromiseWx('getSetting')
+const authorize = toPromiseWx('authorize')
 
 export default {
-  getImageInfo
+  getImageInfo,
+  getUserInfo,
+  getSetting,
+  authorize,
 }
